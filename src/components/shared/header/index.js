9923226ecb35daf49f8a1aa4da437243732a39f6
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2021-10-28 09:20:53 -0300

    Add header layout

---
+++ src/components/shared/header/index.js
@@ -0,0 +1,21 @@
+import './style.css';
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+      return(
+          <div>
+              <div className="header">
+                <div className="logo">Cotizador</div>
+                <div className="buttons">
+                    <h3>Home</h3>
+                    <h3>Settings</h3>
+                    <h3>My profile</h3>
+                </div>
+                </div>
+          </div>
+      )    
+    }
+  }
+
+export default Header;
